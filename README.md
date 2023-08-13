5338c4a39f24f5068f964325c2474c5d67a5b4a4
Eric <you@example.com>
2023-08-13 15:19:03 -0400

    Adding stuff and .gitignore

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # WebChatRoom
+
+#Dependencies
+pip3 install flask
+pip3 install firebase_admin
--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+from flask import Flask, render_template, request, url_for, redirect, send_from_directory
 
+app = Flask(__name__)
+
+#Login page
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    return render_template('./login.html')
